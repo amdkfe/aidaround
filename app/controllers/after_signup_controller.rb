a3965c264d5352c7dd3b951fa7a3b186e1d7cfcc
Ahmad Kafaei <amdkfe@gmail.com>
2018-06-07 18:25:31 +0100

    implemented gps for users location

--- app/controllers/after_signup_controller.rb
+++ app/controllers/after_signup_controller.rb
@@ -1,5 +1,7 @@
 class AfterSignupController < ApplicationController
   
+  respond_to :html, :js
+
   #created controller to set the actions and views of the multi pages after 
   #the signup form and used the Wicked gem to define steps and next paths 
   include Wicked::Wizard
@@ -14,6 +16,11 @@ class AfterSignupController < ApplicationController
   def update
     @user = current_user
     @user.update_attributes(user_params)
+    respond_to do |format|
+      format.html
+      format.json
+    end
+
     render_wizard  @user
   end
 
