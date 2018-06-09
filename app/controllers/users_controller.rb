b6514f7f2ffe718a51dc094c83286783003896a3
Ahmad Kafaei <amdkfe@gmail.com>
2018-06-09 15:09:20 +0100

    user profile restful routing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def new
+    id = current_user.id
+    @user = User.find_by(current_user.id)
+  end
 end
