77db4421bb979f3c48b173ed53ec2c2854a47e8e
Ahmad Kafaei <amdkfe@gmail.com>
2018-06-12 17:13:36 +0100

    working on assigning a task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,16 @@
 class Task < ApplicationRecord
+  before_create :set_tasks_data
 
   belongs_to :owner, class_name: 'User' , foreign_key: "owner_id"
-  belongs_to  :assignee, class_name: 'User', foreign_key: "assignee_id"
+  belongs_to  :assignee, class_name: 'User', foreign_key: "assignee_id", optional: true
+
 
   validates_presence_of :user_id, :latitude, :longitude, :points, :title, :status
+
+  def set_tasks_data
+    self.images ||= ''
+    self.address ||= ''
+    self.title_icon ||= ''
+  end
   
 end
