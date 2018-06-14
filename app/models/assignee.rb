706f5e1ca2916319aff4b2fc79e25d96273de223
Ahmad Kafaei <amdkfe@gmail.com>
2018-06-14 02:21:01 +0100

    fixing avatar bugs on new STI

--- app/models/assignee.rb
+++ app/models/assignee.rb
@@ -1,3 +1,15 @@
 class Assignee < User
 
+ mount_uploader :avatar, AvatarUploader
+
+  validate :avatar_not_changed
+
+     private
+
+  def avatar_not_changed
+    if avatar_changed? && self.persisted?
+      errors.add(:avatar, "Change of avatar not allowed!")
+    end
+  end
+
 end
