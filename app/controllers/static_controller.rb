f62b6bb3b6d6d08104bbdebbee1a421b6d81e1fd
Ahmad Kafaei <amdkfe@gmail.com>
2018-06-14 21:28:04 +0100

    implemented home-page ui

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,6 +2,7 @@ class StaticController < ApplicationController
   def index
 
     @assignees = Assignee.all
+    @top_assignees = Assignee.last(6)
     @tasks = Task.all
     @z= Task.last
     # @task = Task.page(params[:page]).per(5)
