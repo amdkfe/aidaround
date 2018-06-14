3911feff86a11cba4c9a2b8ad201ab810c96f845
Ahmad Kafaei <amdkfe@gmail.com>
2018-06-14 02:46:36 +0100

    fixed avatar bugs on new STI

--- app/assets/javascripts/avatar_uploader.js
+++ app/assets/javascripts/avatar_uploader.js
@@ -1,16 +1,16 @@
-function previewFile(){
-  var preview = document.querySelectorAll('img')[1];
-  var file    = document.getElementById('input').files[0];
-  var reader  = new FileReader();
+//   function previewFile(){
+//   var preview = document.querySelectorAll('img')[1];
+//   var file    = document.getElementById('input').files[0];
+//   var reader  = new FileReader();
 
-  reader.onloadend = function () {
-    preview.src = reader.result;
-  }
-  if (file) {
-    reader.readAsDataURL(file);
-  } else {
-    preview.src = "https://s3.eu-west-2.amazonaws.com/aidaround-avatar-bucket/default_avatar/avad.png";
-  }
-}
+//   reader.onloadend = function () {
+//     preview.src = reader.result;
+//   }
+//   if (file) {
+//     reader.readAsDataURL(file);
+//   } else {
+//     preview.src = "https://s3.eu-west-2.amazonaws.com/aidaround-avatar-bucket/default_avatar/avad.png";
+//   }
+// }
 
-previewFile();  
+// previewFile();  
